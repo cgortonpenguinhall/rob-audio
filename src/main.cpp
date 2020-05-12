70de6c42c77914e487be2c34b833558abaa6e8af
Craig Gorton <cgorton@penguinhall.org>
2020-05-12 11:55:01 -0400

    integrate Arduino Pro Mini with audio
    Co-authored-by: ssoper20 <ssoper20@penguinhall.org>
    Co-authored-by: mlempke20 <mlempke20@penguinhall.org>

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 #include <SPI.h>
 #include <TMRpcm.h>
 
-const int SD_ChipSelectPin = 53;  // yellow, need to change
-const int speakerPin = 46;        // brown, need to change
+const int SD_ChipSelectPin = 10;
+const int speakerPin = 9;
+const int receiverPin = 3;
+const int LED = 7;
 
-TMRpcm audio;
+TMRpcm audio;     
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting");
+  pinMode(receiverPin, INPUT);
+  pinMode(LED, OUTPUT);
 
   audio.speakerPin = speakerPin;
   if (!SD.begin(SD_ChipSelectPin)) {
@@ -19,8 +23,13 @@ void setup() {
   }
   audio.speakerPin = speakerPin;
   audio.setVolume(5);
-  audio.play("APHrob.wav");
 }
 
 void loop() {
+  if ( digitalRead(receiverPin) == HIGH) {
+    audio.play("APHrob.wav");
+    digitalWrite(LED, HIGH);
+    delay(10000);
+    digitalWrite(LED, LOW);
+  } 
 }
